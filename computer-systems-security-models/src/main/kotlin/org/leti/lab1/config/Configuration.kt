6c981431fe0809ae43155e3504aeedf21bf75b78
sstepanov <sstepanov@solanteq.com>
2021-02-17 21:18:31 +0300

    Add default folders

--- computer-systems-security-models/src/main/kotlin/org/leti/lab1/config/Configuration.kt
+++ computer-systems-security-models/src/main/kotlin/org/leti/lab1/config/Configuration.kt
@@ -2,3 +2,6 @@ package org.leti.lab1.config
 
 const val WIDTH = 700.0
 const val HEIGHT = 500.0
+
+val PRIVATE_DIR = System.getProperty("user.home") + "/private"
+val PUBLIC_DIR = System.getProperty("user.home") + "/Desktop/public"
