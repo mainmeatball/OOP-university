829cf4186b9c47796206c2768ff4e9f6c85ac6d2
sstepanov <sstepanov@solanteq.com>
2021-02-17 21:29:31 +0300

    Add file copy functional

---
+++ computer-systems-security-models/src/main/kotlin/org/leti/lab1/service/FileCopyService.kt
@@ -0,0 +1,26 @@
+package org.leti.lab1.service
+
+import org.leti.lab1.component.showPopup
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+
+
+class FileCopyService {
+
+    fun copyFile(pathToSourceFile: String, pathToTargetFile: String) {
+        try {
+            val sourceFile = Paths.get(pathToSourceFile)
+            if (!Files.exists(sourceFile)) {
+                showPopup("Исходный файл $pathToSourceFile не существует.")
+                return
+            }
+            val targetFile = Paths.get(pathToTargetFile)
+            Files.copy(sourceFile, targetFile)
+        } catch (e: IOException) {
+            showPopup("Ошибка.\n $e")
+            return
+        }
+        showPopup("Копирование прошло успешно")
+    }
+}
