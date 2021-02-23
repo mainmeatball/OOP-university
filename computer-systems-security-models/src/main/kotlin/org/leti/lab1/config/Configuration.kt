3eb911733241a66a8d1a490046c201e11015f4ec
sstepanov <sstepanov@solanteq.com>
2021-02-23 16:32:18 +0300

    Change configuration

--- computer-systems-security-models/src/main/kotlin/org/leti/lab1/config/Configuration.kt
+++ computer-systems-security-models/src/main/kotlin/org/leti/lab1/config/Configuration.kt
@@ -3,6 +3,6 @@ package org.leti.lab1.config
 const val WIDTH = 700.0
 const val HEIGHT = 500.0
 
-val PRIVATE_DIR = System.getProperty("user.home") + "/private"
+val PRIVATE_DIR = System.getProperty("user.home") + "/Desktop/private"
 val PUBLIC_DIR = System.getProperty("user.home") + "/Desktop/public"
 val INTRUDER_DIR = System.getProperty("user.home") + "/Desktop/intruder"
